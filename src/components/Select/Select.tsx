cdf0e578ef536e662f1c95c9bf8659e5ff8485a9
isabelxklee <hi@kleetime.com>
2024-02-19 22:04:22 -0500

    created select component

---
+++ src/components/Select/Select.tsx
@@ -0,0 +1,11 @@
+import React, { ReactElement } from 'react';
+
+export const Select = (): ReactElement => {
+  return (
+    <>
+      <input type="select">
+        <option value="All" />
+      </input>
+    </>
+  );
+};
