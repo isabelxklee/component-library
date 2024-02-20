cda57bc7a45631f87a2b728347d78af65b829d85
isabelxklee <hi@kleetime.com>
2024-02-19 22:13:25 -0500

    updated select

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -3,9 +3,10 @@ import React, { ReactElement } from 'react';
 export const Select = (): ReactElement => {
   return (
     <>
-      <input type="select">
-        <option value="All" />
-      </input>
+      <label>Example select</label>
+      <select name="Example name">
+        <option value="All">All</option>
+      </select>
     </>
   );
 };
